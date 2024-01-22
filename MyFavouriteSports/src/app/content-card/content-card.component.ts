c499f2f405991f3c65e91857ef6520f894be83f0
Sajan81 <122993112+Sajan81@users.noreply.github.com>
2024-01-21 23:39:43 -0500

    Item names
    i

--- MyFavouriteSports/src/app/content-card/content-card.component.ts
+++ MyFavouriteSports/src/app/content-card/content-card.component.ts
@@ -13,9 +13,9 @@ export class ContentCardComponent implements OnInit {
   title="My Application"
   constructor() {
     // Adding at least 3 valid items
-    this.contentList.add({ id: 1, title: 'Item 1', description: 'Description 1', creator: 'Creator 1', imgURL: 'https://wallpapercave.com/wp/2mFQbur.jpg', type: 'Type 1',tags: ['tag1', 'tag2', 'tag3'] });
-    this.contentList.add({ id: 2, title: 'Item 2', description: 'Description 2', creator: 'Creator 2', imgURL: 'https://filmdaily.co/wp-content/uploads/2020/12/cricket_lede.jpg', type: 'Type 2' });
-    this.contentList.add({ id: 3, title: 'Item 3', description: 'Description 3', creator: 'Creator 3', imgURL: 'https://images.hdqwalls.com/wallpapers/basketball-hd.jpg', type: 'Type 3' });
+    this.contentList.add({ id: 1, title: ' Online Game', description: 'Description 1', creator: 'Creator 1', imgURL: 'https://wallpapercave.com/wp/2mFQbur.jpg', type: 'Type 1',tags: ['tag1', 'tag2', 'tag3'] });
+    this.contentList.add({ id: 2, title: 'Cricket', description: 'Description 2', creator: 'Creator 2', imgURL: 'https://filmdaily.co/wp-content/uploads/2020/12/cricket_lede.jpg', type: 'Type 2' });
+    this.contentList.add({ id: 3, title: ' BasketBall', description: 'Description 3', creator: 'Creator 3', imgURL: 'https://images.hdqwalls.com/wallpapers/basketball-hd.jpg', type: 'Type 3' });
   }
 
   ngOnInit(): void {
